8c60d6bec3ef158f58656ec46d4cedf9bc4f6559
Peter Giri <70839531+petergiri@users.noreply.github.com>
2021-11-13 17:49:41 +0700

    Add files via upload

---
+++ Program Skripsi New/registrasi.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman Registrasi</title>
+    <style></style>
+</head>
+
+<body>
+
+    <h1>Halaman Registrasi</h1>
+
+    <form action="" method="post">
+
+        <ul>
+            <li>
+                <label for="username">username</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">password</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <label for="password2">konfirmasi password</label>
+                <input type="password" name="password2" id="password2">
+            </li>
+            <li>
+                <label for="email">email</label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <button type="submit" name="Register">Register</button>
+            </li>
+
+        </ul>
+
+    </form>
+
+
+</body>
+
+</html>
